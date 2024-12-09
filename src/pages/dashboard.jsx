51597d26c15dc115ad48ffbd3fce7548916b915b
AchmadAkbarR <111202213959@mhs.dinus.ac.id>
2024-12-09 17:47:09 +0700

    week11

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,6 +1,6 @@
 import Card from "../components/Elements/Card";
 import MainLayout from "../components/Layouts/MainLayout";
-import CardBill from "../components/Fragments/Dashboard/CardBill";
+import CardBill from "../components/Fragments/Dashboard/CardBull";
 import CardExpenseBreakdown from "../components/Fragments/Dashboard/CardExpenseBreakdawn";
 import CardTransaction from "../components/Fragments/Dashboard/CardTransaction.jsx";
 import CardBalance from "../components/Fragments/Dashboard/CardBalance";
